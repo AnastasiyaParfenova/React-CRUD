8c13ea8974af64a8e66fb6df00cd061cee405c3b
Anastasiya <parfenova.anastasiya.98@yandex.ru>
2024-03-11 13:22:41 +0500

    implementation of the search and filtering function

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+
 import './employees-add-form.css';
 
 class EmployeesAddForm extends Component {
@@ -12,12 +13,14 @@ class EmployeesAddForm extends Component {
 
     onValueChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name] : e.target.value
         })
     }
 
     onSubmit = (e) => {
         e.preventDefault();
+        
+        if (this.state.name.length < 3 || !this.state.salary) return;
         this.props.onAdd(this.state.name, this.state.salary);
         this.setState({
             name: '',
@@ -32,22 +35,23 @@ class EmployeesAddForm extends Component {
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit = {this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
                         name="name"
-                        value={name}   
+                        value={name} 
                         onChange={this.onValueChange}/>
                     <input type="number"
                         className="form-control new-post-label"
-                        placeholder="З/П в $?" 
+                        placeholder="З/П в $?"
                         name="salary"
-                        value={salary}
+                        value={salary} 
                         onChange={this.onValueChange}/>
     
                     <button type="submit"
-                            className="btn btn-outline-light" onClick={this.onSubmit}>Добавить</button>
+                            className="btn btn-outline-light">Добавить</button>
                 </form>
             </div>
         )
